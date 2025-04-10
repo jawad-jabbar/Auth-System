f89de48331a4fd9851122e7e0339d84f466ec6b2
Jawad Jabbar <jawadjabbar90@gmail.com>
2025-04-10 11:30:38 +0500

    Added Mongodb Cluster

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -24,7 +24,7 @@ const postController = {
 
       return res.status(200).json({ posts });
     } catch (error) {
-      return res.status(500).json({ message:"Error fetching all posts.", error: error.message });
+      return res.status(500).json({ message:"Error while fetching all posts.", error: error.message });
     }
   },
 };
